ece884711827d0b24e529e5ac2fe5df350aae6a0
arjun <koriarjun30@gmail.com>
2017-05-31 17:00:34 +0530

    Client code added for publish

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,37 @@ var express = require('express');
 var router = express.Router();
 var utility = require('./utility');
 
-router.get('/', function(req, res, next) {
+router.get('/pub', function(req, res, next) {
+
+    var publishConfig = {
+        channel: "PortallChatBot_Channel",
+        message: {
+            data: {
+                chatMsg: "how do I search BL Number ?",
+                botMsg:true
+            }
+        }
+    }
+    pubnub.publish(publishConfig, function(status, response) {
+        console.log(status, response);
+    });
+
+    pubnub.addListener({
+        status: function(statusEvent) {
+            if (statusEvent.category === "PNConnectedCategory") {
+            }
+        },
+        message: function(message) {
+            console.log("New Message!!", message);
+        },
+        presence: function(presenceEvent) {
+            // handle presence
+        }
+    });
+    console.log("Subscribing. CLIENT.");
+    pubnub.subscribe({
+        channels: ['PortallChatBot_Channel']
+    });
 })
 module.exports = router;
 
